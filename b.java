1a18bd16b1f4591dddd97f1bd06d1d20127b6310
Brian Okungu <92260552+br-cmd@users.noreply.github.com>
2023-03-21 22:27:31 +0300

    Add files via upload

---
+++ b.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginpage;
+
+/**
+ *
+ * @author Brian Okungu
+ */
+public class b {
+
+    public b(String unit1, String unit2, String unit3, String unit4, String unit5, String unit6, String unit7) {
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
